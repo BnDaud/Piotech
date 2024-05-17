35af45097c09b0b1c0303bb70382a75183800f0d
Bndaud <Lawalsulaimon70@gmail.com>
2024-05-17 10:13:45 +0100

    Updated login page interface

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,17 @@
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-
+import { FcGoogle } from "react-icons/fc";
 export default function Login() {
   return (
     <>
-      <div class=" w-96 mt-5">
+      <div class=" w-96 mt-5 bg-rose-400">
         <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className=" ml-32"> Sign In</div>
+          <div className="mt-3  mb-10 cursor-pointer mx-auto flex flex-wrap border border-black rounded-lg pl-14 py-2">
+            {" "}
+            <FcGoogle className="text-2xl " />
+            <span className="ml-3"> Sign In With Google</span>
+          </div>
           <div class="mb-4">
-            <label class="block text-gray-700 text-sm  mb-2" for="email">
+            <label class="block text-gray-700 text-sm  mb-2 " for="email">
               Email
             </label>
             <input
@@ -29,12 +33,14 @@ export default function Login() {
             />
           </div>
 
-          <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-          >
-            Sign In
-          </button>
+          <div className=" flex">
+            <button
+              class=" mx-auto  w-full block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-3"
+              type="button"
+            >
+              Sign In
+            </button>
+          </div>
 
           <div>
             <a
--- src/page/login.js
+++ src/page/login.js
@@ -1,4 +1,3 @@
-import { FcGoogle } from "react-icons/fc";
 import Login from "../components/login";
 
 export default function Loginpage() {
@@ -8,12 +7,6 @@ export default function Loginpage() {
         <div className=" basis-2/6"> </div>
         <div className="basis-2/6 container mx-auto px-4 ">
           <div className="mt-3 flex flex-col justify-center ">
-            <div className=" mx-auto"> Sign In</div>
-            <div className="mt-3   cursor-pointer mx-auto flex flex-wrap">
-              {" "}
-              <FcGoogle className="text-2xl" />
-              <span className="ml-3"> Sign In With Google</span>
-            </div>
             <div className="mx-auto ">
               <Login />
             </div>
